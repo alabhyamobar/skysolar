f5ab036c8e095ab9b0e0afd49f492ee32b8691a0
alabhyamobar <alabhyamobar50@gmail.com>
2026-04-23 11:53:00 +0530

    service deone

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -185,7 +185,7 @@ const Services = () => {
         ref={serviceRef} 
         className="w-full h-screen flex flex-col-reverse lg:pl-30 lg:flex-row lg:items-center lg:justify-between gap-10"
       >
-        <div className="flex flex-col justify-content h-[70vh]">
+        <div className="flex flex-col justify-content h-[70vh] pl-5 lg:p-0">
           {servicesData.map((service, index) => (
             <div
               key={index}
