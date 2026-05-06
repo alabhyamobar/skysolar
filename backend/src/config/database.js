76d434a07d4648fbe4f93adfca3a69f57b10a83f
alabhyamobar <alabhyamobar50@gmail.com>
2026-05-06 22:22:45 +0530

    .

--- backend/src/config/database.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose"
-import config from "./config.js"
-
-export default async function connectDb() {
-    await mongoose.connect(config.MONGO_URI);
-    console.log("database connected successfully");
-}
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,11 @@
-import React, { useEffect } from "react";
-import Services from "./pages/Services";
-import Landing from "./pages/Landing";
-import NAV from "./Components/NAV";
-import { Route, Routes } from "react-router-dom";
-import Gallary from "./pages/Gallary";
-import Calculator from "./pages/Calculator";
-import Contact from "./pages/Contact";
-import About from "./pages/About";
-import Loader from "./Components/Loader";
-import LocomotiveScroll from 'locomotive-scroll';
-
+import React from 'react'
 
 const App = () => {
-
-const locomotiveScroll = new LocomotiveScroll();
-
   return (
-    <Loader>
-      <div className="w-screen overflow-x-hidden">
-        <NAV />
+    <div className='h-screen w-screen'>
+      
+    </div>
+  )
+}
 
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div className="w-full  overflow-x-hidden">
-                <Landing />
-                <About />
-              </div>
-            }
-          />
-          <Route path="/calculator" element={<Calculator />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Loader>
-  );
-};
-
-export default App;
+export default App
