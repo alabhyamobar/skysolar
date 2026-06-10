7e00fd27a013544b341973ba093fdc6a763ffcb9
alabhyamobar <alabhyamobar50@gmail.com>
2026-06-11 02:50:24 +0530

    ...

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors({
-    origin: [ "http://localhost:5173","https://skysolar-two.vercel.app/"],
-    methods: ["GET" , "POST" , "PUT" , "DELETE"],
-    credentials:true 
+    origin: ["https://skysolar-two.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
 }))
 
 
