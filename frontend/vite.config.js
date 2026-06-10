5b3a2c4ee6e67a4094a2ecb5a975293444376440
alabhyamobar <alabhyamobar50@gmail.com>
2026-06-11 02:41:22 +0530

    vercel related changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
-  base: '/skysolar/',
   plugins: [
     tailwindcss(),
-    glsl()
   ],
-  optimizeDeps: {
-    exclude:["sheryjs"]
-  }
 })
