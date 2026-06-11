49fc7ddf2991756392b04a134a783c2360fc2344
alabhyamobar <alabhyamobar50@gmail.com>
2026-06-11 16:10:52 +0530

    limitter revoked

--- backend/src/modules/user/user.routes.js
+++ backend/src/modules/user/user.routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const {createQueryController} = require("../user/user.controller.js")
-const queryLimiter = require("../../middleware/ratelimitter.js")
+
 
 const userRouter = express.Router()
-userRouter.post("/query" ,queryLimiter, createQueryController)
+userRouter.post("/query" , createQueryController)
 
 module.exports = userRouter
